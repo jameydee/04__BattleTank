cb01f4bf06f537b2b8e257d0ce65d041bfa00475
JD <jameydee@gmail.com>
2016-08-02 17:32:32 -0500

    Added BeginPlay() override to TankPlayerController.

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,9 +3,17 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+}
+
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast <ATank> (GetPawn());
+
 }
 
 
